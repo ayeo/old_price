e6dcad190cddc5fea75d13de311858f6f069ef4d
ayeo <me@ayeo.pl>
2014-12-13 22:33:23 +0100

    README code documented

--- README.md
+++ README.md
@@ -3,26 +3,29 @@ Price
 
 Price handling. This library is under development. Do not use :)
 
-### Usage
+### Basic Usage
 ```
 use \Price\Builder\Builder as PriceBuilder;
 use \Price\Decorator\Round as RoundDecorator;
 
-$builder = new PriceBuilder();
-$priceA = $builder->buildByNett(79.104, 10);
-$priceA->multiply(2)->getNett(); //returns 158.208
+$rawBuilder = new PriceBuilder();
+$price = $rawBuilder->buildByNett(79.104, 10);
+$price->multiply(2)->getNett(); //returns 158.208
 
-$builder->decorate($priceA, new RoundDecorator(2))->getNett(); //returns 158.21
+//round existing Price
+$roundedBuilder = new PriceBuilder();
+$roundedBuilder->addDecorator(new RoundDecorator(2));
+$roundedBuilder->decorate($price)->getNett(); //returns 158.21
 ```
 
-### Use rounded values to calculations
+### Use rounded values for calculations
 ```
-$builder = new PriceBuilder();
-$builder->setCurrencySymbol('USD');
-$builder->addDecorator(new RoundDecorator(2));
+$usdRoundedBuilder = new PriceBuilder();
+$usdRoundedBuilder->setCurrencySymbol('USD');
+$usdRoundedBuilder->addDecorator(new RoundDecorator(2));
 
-$priceA = $builder->buildByNett(79.104, 10);
-$priceA->multiply(2)->getNett(); //returns 158.20
+$price = $usdRoundedBuilder->buildByNett(79.104, 10);
+$price->multiply(2)->getNett(); //returns 158.20 USD
 ```
 
 ### Converting currency
--- index.php
+++ index.php
@@ -1,42 +1,12 @@
 <?php
 require('./vendor/autoload.php');
 
-use Price\Builder\Builder as PriceBuilder;
-use Price\Decorator\ConvertCurrency as ConvertCurrencyDecorator;
-use Price\Decorator\Round as RoundDecorator;
+use \Price\Builder\Builder as PriceBuilder;
+use \Price\Decorator\Round as RoundDecorator;
 
-$rates = ['USD' => 1.000, 'PLN' => 3.3543];
-$convertCurrencyDecorator = new ConvertCurrencyDecorator('USD', $rates);
+$usdRoundedBuilder = new PriceBuilder();
+$usdRoundedBuilder->setCurrencySymbol('USD');
+$usdRoundedBuilder->addDecorator(new RoundDecorator(2));
 
-$defaultRoundDecorator = new RoundDecorator(2);
-
-$usdBuilder = new PriceBuilder();
-$usdBuilder->addDecorator($defaultRoundDecorator);
-$usdBuilder->addDecorator($convertCurrencyDecorator);
-$usdBuilder->addDecorator($defaultRoundDecorator);
-$usdBuilder->setCurrencySymbol('PLN');
-
-$price = $usdBuilder->buildByNett(100.00, 10); //gross: 32.79 USD
-$price->multiply(2); //gross: 65.58 USD
-
-
-var_dump($price->getGross());
-var_dump($price->getNett());
-var_dump($price->getCurrencySymbol());
-
-
-
-
-var_dump('-----------------------------------');
-
-$plnBuilder = new PriceBuilder();
-$plnBuilder->addDecorator($defaultRoundDecorator);
-$plnBuilder->setCurrencySymbol('PLN');
-
-$price = $plnBuilder->buildByNett(100.00, 10); //gross: 110.00 PLN
-$price->multiply(2); //gross: 220.00 PLN
-$price = $usdBuilder->decorate($price); //gross: 65.59 USD
-
-var_dump($price->getGross());
-var_dump($price->getNett());
-var_dump($price->getCurrencySymbol());
+$price = $usdRoundedBuilder->buildByNett(79.104, 10);
+$price->multiply(2)->getNett(); //returns 158.20
